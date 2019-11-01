6e3d210150734b1496640a6a02813494ae52acbe
berkkoylu <berkkoylu@marun.edu.tr>
2019-11-02 00:05:28 +0300

    refactor packages

---
+++ src/com/company/Player_die/Dice.java
@@ -0,0 +1,22 @@
+package com.company.Player_die;
+import java.util.Random;
+
+public class Dice {
+
+    //Property of Dice
+    private int faceValue;
+    private Random rand;
+    public Dice(){
+        rand = new Random();
+        faceValue = rand.nextInt(6) + 1;
+    }
+    //This method setting the dice values
+    public void setFaceValue() {
+        faceValue = rand.nextInt(6) + 1;
+    }
+
+    //returns face value of the dice
+    public int getFaceValue() {
+        return faceValue;
+    }
+}
