92689b0ab173675367348db2fdf3a74cb7bbd947
erayayaz <eray19ayaz@gmail.com>
2019-11-30 00:43:31 +0300

    Cards are added(2)

---
+++ src/com/company/board/LuckCard.java
@@ -0,0 +1,43 @@
+package com.company.board;
+
+import com.company.Player_die.Player;
+
+public class LuckCard extends Square {
+
+
+    public LuckCard(String name, int index) {
+
+        super(name, index);
+        //
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    public void setName(String name) {
+        super.setName(name);
+    }
+
+    @Override
+    public void setIndex(int index) {
+        super.setIndex(index);
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public int getIndex() {
+        return super.getIndex();
+    }
+
+    @Override
+    public void action(Player player) {
+        Luck.pickCard(player);
+    }
+}
